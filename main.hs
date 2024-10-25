60b9054897b153555ed0559e61c257f489f7407b
Gui <guiziifs@gmail.com>
2024-10-24 23:19:13 -0300

    Update Directories

--- main.hs
+++ main.hs
@@ -1,91 +1,75 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
-import Network.Wai.Middleware.RequestLogger (logStdoutDev, logStdout)
-import System.Random (randomRIO, Random (random))
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import System.Random (randomRIO)
 import Control.Monad.IO.Class (liftIO)
-import qualified  Data.Text.Lazy as TL
+import qualified Data.Text.Lazy as TL
 import Network.Wai.Middleware.Static (staticPolicy, addBase)
+import Data.Text.Lazy (replace)
+import System.IO (readFile)
 
--- funcao para carregar os filmes do arquivo
+-- Função para carregar os arquivos CSV
 loadFile :: FilePath -> IO [String]
 loadFile filePath = do
     content <- readFile filePath
     return (lines content)
 
--- funcao que seleciona um filme aleatoriamente
+-- Função que seleciona um item aleatoriamente
 randomFile :: [String] -> IO String
-randomFile movies = do
-    index <- randomRIO (0, length movies - 1)
-    return (movies !! index)
+randomFile items = do
+    index <- randomRIO (0, length items - 1)
+    return (items !! index)
 
-main :: IO()
+-- Função para carregar templates HTML e substituir o placeholder
+loadTemplate :: FilePath -> IO TL.Text
+loadTemplate filePath = do
+    content <- readFile filePath
+    return (TL.pack content)
+
+-- Função para substituir a recomendação no template
+replacePlaceholder :: TL.Text -> String -> TL.Text
+replacePlaceholder template recommendation = replace "{{recommendation}}" (TL.pack recommendation) template
+
+main :: IO ()
 main = do
-    -- define o nome dos arquivos
-    let csvMovie = "filmes.csv"
-    let csvSerie = "series.csv"
-    let csvSong = "musicas.csv"
+    -- Define o nome dos arquivos CSV na pasta "data"
+    let csvMovie = "data/filmes.csv"
+    let csvSerie = "data/series.csv"
+    let csvSong = "data/musicas.csv"
 
-    -- carrega os arquivos
+    -- Carrega os arquivos CSV
     movies <- loadFile csvMovie
     series <- loadFile csvSerie
     songs <- loadFile csvSong
 
-    -- inicia servidor Web
+    -- Carrega os templates HTML
+    indexTemplate <- liftIO $ loadTemplate "templates/index.html"
+    recommendationTemplate <- liftIO $ loadTemplate "templates/recommendation.html"
+
+    -- Inicia o servidor Web
     scotty 3000 $ do
         middleware logStdoutDev
-        middleware $ staticPolicy (addBase "static")
-        
-        get "/" $ do
-            html $
-                "<html><head>" <>
-                "<link rel='stylesheet' type='text/css' href='/style.css'>"<>
-                "</head><body>" <>
-                "<h1>Escolha uma recomendação: </h1>" <> 
-                "<form action='/random-movie' method ='get'>" <>
-                "<button type='submit'>Recomendar um filme</button>" <>
-                "</form>" <>
-                "<form action='/random-song' method ='get'>" <>
-                "<button type='submit'>Recomendar uma Musica</button>" <>
-                "</form>" <>
-                "<form action='/random-serie' method ='get'>" <>
-                "<button type='submit'>Recomendar uma Serie</button>" <>
-                "</form>" <>
-                "</body></html>"
+        middleware $ staticPolicy (addBase "static")  -- Servir arquivos estáticos da pasta "static"
 
+        -- Página inicial com formulários
+        get "/" $ do
+            html indexTemplate
 
         -- Rota para obter uma música aleatória
         get "/random-song" $ do
             musicaIO <- liftIO $ randomFile songs
-            html $
-                "<html><head>" <>
-                "<link rel='stylesheet' type='text/css' href='/style.css'>" <>
-                "</head><body>" <>
-                "<div class='recommendation-container'>" <>
-                "<h3>Música Recomendada: " <> TL.pack musicaIO <> "</h3>" <>
-                "<a href='/'>Voltar</a>" <>
-                "</div></body></html>"
+            let page = replacePlaceholder recommendationTemplate musicaIO
+            html page
 
         -- Rota para obter um filme aleatório
         get "/random-movie" $ do
             filmeIO <- liftIO $ randomFile movies
-            html $
-                "<html><head>" <>
-                "<link rel='stylesheet' type='text/css' href='/style.css'>" <>
-                "</head><body>" <>
-                "<div class='recommendation-container'>" <>
-                "<h3>Filme Recomendado: " <> TL.pack filmeIO <> "</h3>" <>
-                "<a href='/'>Voltar</a>" <>
-                "</div></body></html>"
+            let page = replacePlaceholder recommendationTemplate filmeIO
+            html page
 
         -- Rota para obter uma série aleatória
         get "/random-serie" $ do
             serieIO <- liftIO $ randomFile series
-            html $
-                "<html><head>" <>
-                "<link rel='stylesheet' type='text/css' href='/style.css'>" <>
-                "</head><body>" <>
-                "<div class='recommendation-container'>" <>
-                "<h3>Série Recomendada: " <> TL.pack serieIO <> "</h3>" <>
-                "<a href='/'>Voltar</a>" <>
-                "</div></body></html>"
+            let page = replacePlaceholder recommendationTemplate serieIO
+            html page
